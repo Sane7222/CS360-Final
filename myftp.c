5baf51ae753e224c22372e928dfb022bcd5950b0
Matias M <matias.moseley@gmail.com>
2022-11-27 15:44:49 -0800

    Client sends message to Server

--- myftp.c
+++ myftp.c
@@ -9,16 +9,16 @@
 #include<errno.h>
 #include<sys/types.h>
 #include<sys/socket.h>
-#include<sys/wait.h>
 #include<netdb.h>
 
 #define PORT_NUM "49999"
 #define BACKLOG 1
+#define BUFFER_SIZE 512
 
 void main(int argc, char *argv[]){
 
-    char buf[32] = {0};
-    int connectfd, errNum, numRead;
+    char buffer[BUFFER_SIZE] = {0}, *exitCond = "exit\0";
+    int connectfd, errNum, num;
     struct addrinfo cliAddr = {0}, *this;
 
     cliAddr.ai_family = AF_INET;
@@ -27,15 +27,23 @@ void main(int argc, char *argv[]){
     if((errNum = getaddrinfo(argv[1], PORT_NUM, &cliAddr, &this)) != 0){ // Get internet addr of argv[1]
         fprintf(stderr, "Error: %s\n", gai_strerror(errNum));
         freeaddrinfo(this);
-        exit(1); }
+        exit(1);
+    }
 
     if((connectfd = socket(this->ai_family, this->ai_socktype, 0)) == -1 || connect(connectfd, this->ai_addr, this->ai_addrlen) == -1){ // Create a socket and connect it to server port
         fprintf(stderr, "Error: %s\n", strerror(errno));
         freeaddrinfo(this);
-        exit(1); }
+        exit(1);
+    }
 
     freeaddrinfo(this);
+    
+    for(;;){
+        fprintf(stdout, "Please enter command:\n");
+        num = read(0, buffer, BUFFER_SIZE);
+        buffer[num-1] = '\0';
+        write(connectfd, buffer, num); // Write command to server
 
-    numRead = read(connectfd, buf, sizeof(buf)); // Read from server
-    fprintf(stdout, "%s", buf);
+        if(!strncmp(buffer, exitCond, num)) exit(0); // Exit recieved
+    }
 }
--- myftpserve.c
+++ myftpserve.c
@@ -11,18 +11,18 @@
 #include<sys/socket.h>
 #include<sys/wait.h>
 #include<netdb.h>
-#include<time.h>
 
 #define PORT_NUM "49999"
 #define BACKLOG 1
+#define BUFFER_SIZE 512
 
 void main(int argc, char *argv[]){
 
-    char host[NI_MAXHOST], buf[32] = {0};
-    int listenfd, connectfd, errNum, log = 0;
+    char host[NI_MAXHOST], buffer[BUFFER_SIZE] = {0}, *exitCond = "exit\0";
+    int listenfd, connectfd, errNum, num, log = 0;
     struct addrinfo servAddr = {0}, *this;
     struct sockaddr_storage clientAddr;
-    time_t date; socklen_t addrlen; pid_t here;
+    socklen_t addrlen = sizeof(struct sockaddr_storage); pid_t here;
 
     servAddr.ai_family = AF_INET;
     servAddr.ai_socktype = SOCK_STREAM;
@@ -30,34 +30,38 @@ void main(int argc, char *argv[]){
     if((errNum = getaddrinfo(NULL, PORT_NUM, &servAddr, &this)) != 0){ // Get internet addr
         fprintf(stderr, "Error: %s\n", gai_strerror(errNum));
         freeaddrinfo(this);
-        exit(1); }
+        exit(1);
+    }
 
     // Make a reuseable socket, bind it to a port, and establish as a server 
     if((listenfd = socket(this->ai_family, this->ai_socktype, 0)) == -1 || setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) == -1 || bind(listenfd, this->ai_addr, this->ai_addrlen) == -1 || listen(listenfd, BACKLOG) == -1){
         fprintf(stderr, "Error: %s\n", strerror(errno));
         freeaddrinfo(this);
-        exit(1); }
+        exit(1);
+    }
 
     freeaddrinfo(this);
 
     for(;;){
-        addrlen = sizeof(struct sockaddr_storage);
         if((connectfd = accept(listenfd, (struct sockaddr *) &clientAddr, &addrlen)) == -1){ // Accept connection
             fprintf(stderr, "Error: %s\n", strerror(errno));
-            exit(1); }
+            exit(1);
+        }
 
         log++;
         if((here = fork()) == -1){ // Fork
             fprintf(stderr, "Error: %s\n", strerror(errno));
             close(connectfd);
-            exit(1); }
+            exit(1);
+        }
 
         else if(here){ // Parent
             close(connectfd);
 
             if(waitpid(-1, NULL, WNOHANG) == -1){ // Clean up child
                 fprintf(stderr, "Error: %s\n", strerror(errno));
-                exit(1); }
+                exit(1);
+            }
         }
 
         else{ // Child
@@ -65,14 +69,19 @@ void main(int argc, char *argv[]){
 
             if((errNum = getnameinfo((struct sockaddr *) &clientAddr, addrlen, host, NI_MAXHOST, NULL, 0, NI_NUMERICSERV)) != 0){ // Convert socket addr to host name
                 fprintf(stderr, "Error: %s\n", gai_strerror(errNum));
-                exit(1); }
+                exit(1);
+            }
 
             fprintf(stdout, "%s %d\n", host, log);
 
-            date = time(NULL);
-            snprintf(buf, sizeof(buf), "%.18s\n", ctime(&date)); // Date and time
-
-            write(connectfd, buf, sizeof(buf)); // Write to client and close connection
-            exit(0); }
+            for(;;){
+                num = read(connectfd, buffer, BUFFER_SIZE); // Write to client and close connection
+                fprintf(stdout, "%s\n", buffer);
+                if(!strncmp(buffer, exitCond, num)){  // Exit recieved
+                    fprintf(stdout, "Server Child Process Exiting\n");
+                    exit(0);
+                }
+            }
+        }
     }
 }
